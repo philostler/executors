e6a560815e1953443dda7c80d51d7c86c0a33339
Phil Ostler <philostler@gmail.com>
2011-01-27 23:39:09 +0000

    Initial commit.

---
+++ test/executors_test.rb
@@ -0,0 +1,3 @@
+require "test_helper"
+
+require "factory_test"
---
+++ test/factory_test.rb
@@ -0,0 +1,72 @@
+require "test_helper"
+
+java_import java.util.concurrent.ExecutorService
+java_import java.util.concurrent.ScheduledExecutorService
+
+class FactoryTest < Test::Unit::TestCase
+  def test_get_cached_executor
+    executor = Executors::Factory.get_cached_executor
+    assert_not_nil executor
+    assert executor.java_kind_of? ExecutorService
+  end
+
+  def test_get_fixed_executor
+    executor = Executors::Factory.get_fixed_executor 1
+    assert_not_nil executor
+    assert executor.java_kind_of? ExecutorService
+
+    executor = Executors::Factory.get_fixed_executor 1000
+    assert_not_nil executor
+    assert executor.java_kind_of? ExecutorService
+
+    executor = Executors::Factory.get_fixed_executor "99 string"
+    assert_not_nil executor
+    assert executor.java_kind_of? ExecutorService
+
+    assert_raise ArgumentError do
+      Executors::Factory.get_fixed_executor nil
+    end
+    assert_raise ArgumentError do
+      Executors::Factory.get_fixed_executor 0
+    end
+    assert_raise ArgumentError do
+      Executors::Factory.get_fixed_executor "string"
+    end
+  end
+
+  def test_get_scheduled_executor
+    executor = Executors::Factory.get_scheduled_executor 1
+    assert_not_nil executor
+    assert executor.java_kind_of? ScheduledExecutorService
+
+    executor = Executors::Factory.get_scheduled_executor 1000
+    assert_not_nil executor
+    assert executor.java_kind_of? ScheduledExecutorService
+
+    executor = Executors::Factory.get_scheduled_executor "99 string"
+    assert_not_nil executor
+    assert executor.java_kind_of? ScheduledExecutorService
+
+    assert_raise ArgumentError do
+      Executors::Factory.get_scheduled_executor nil
+    end
+    assert_raise ArgumentError do
+      Executors::Factory.get_scheduled_executor 0
+    end
+    assert_raise ArgumentError do
+      Executors::Factory.get_scheduled_executor "string"
+    end
+  end
+
+  def test_get_single_executor
+    executor = Executors::Factory.get_single_executor
+    assert_not_nil executor
+    assert executor.java_kind_of? ExecutorService
+  end
+
+  def test_get_single_scheduled_executor
+    executor = Executors::Factory.get_single_scheduled_executor
+    assert_not_nil executor
+    assert executor.java_kind_of? ScheduledExecutorService
+  end
+end
