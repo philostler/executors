367a1cd288433b33e3b80eccb39afa93473d2333
Phil Ostler <philostler@gmail.com>
2011-01-27 23:52:03 +0000

    Some code cleanup, fixed nil size error, corrected test failure with scheduled executor when string is passed as size and size.to_i now always called.

--- lib/executors/factory.rb
+++ lib/executors/factory.rb
@@ -9,43 +9,43 @@ module Executors
       SINGLE_SCHEDULED_EXECUTOR = "single_scheduled"
 
       # Returns a cached ExecutorService[http://download.oracle.com/javase/6/docs/api/java/util/concurrent/Executors.html#newCachedThreadPool()].
-      def get_cached_executor()
+      def get_cached_executor
         get_executor CACHED_EXECUTOR
       end
 
       # Returns a fixed size ExecutorService[http://download.oracle.com/javase/6/docs/api/java/util/concurrent/Executors.html#newFixedThreadPool(int)].
-      def get_fixed_executor(size)
+      def get_fixed_executor size
         get_executor FIXED_EXECUTOR, size
       end
 
       # Returns a fixed size ScheduledExecutorService[http://download.oracle.com/javase/6/docs/api/java/util/concurrent/Executors.html#newScheduledThreadPool(int)].
-      def get_scheduled_executor(size)
+      def get_scheduled_executor size
         get_executor SCHEDULED_EXECUTOR, size
       end
 
       # Returns a single threaded ExecutorService[http://download.oracle.com/javase/6/docs/api/java/util/concurrent/Executors.html#newSingleThreadExecutor()].
-      def get_single_executor()
+      def get_single_executor
         get_executor SINGLE_EXECUTOR
       end
 
       # Returns a single threaded ScheduledExecutorService[http://download.oracle.com/javase/6/docs/api/java/util/concurrent/Executors.html#newSingleThreadScheduledExecutor()].
-      def get_single_scheduled_executor()
+      def get_single_scheduled_executor
         get_executor SINGLE_SCHEDULED_EXECUTOR
       end
 
       protected
-      def get_executor(type, size)
+      def get_executor type, size = nil
         case type
           when CACHED_EXECUTOR
             return java.util.concurrent.Executors.new_cached_thread_pool
           when FIXED_EXECUTOR
             raise ArgumentError, "size cannot be nil" unless size
             raise ArgumentError, "size must be larger than 0" unless size.to_i > 0
-            return java.util.concurrent.Executors.new_fixed_thread_pool size
+            return java.util.concurrent.Executors.new_fixed_thread_pool size.to_i
           when SCHEDULED_EXECUTOR
             raise ArgumentError, "size cannot be nil" unless size
-            raise ArgumentError, "size must not be less than 0" unless size.to_i >= 0
-            return java.util.concurrent.Executors.new_scheduled_thread_pool size
+            raise ArgumentError, "size must be larger than 0" unless size.to_i > 0
+            return java.util.concurrent.Executors.new_scheduled_thread_pool size.to_i
           when SINGLE_EXECUTOR
             return java.util.concurrent.Executors.new_single_thread_executor
           when SINGLE_SCHEDULED_EXECUTOR
