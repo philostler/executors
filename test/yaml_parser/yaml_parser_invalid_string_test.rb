7d4b52b826f955a3306c3bd5ca24028c643a68e7
Phil Ostler <philostler@gmail.com>
2011-02-06 12:19:43 +0000

    Initial commit.

---
+++ test/yaml_parser/yaml_parser_invalid_string_test.rb
@@ -0,0 +1,14 @@
+require "yaml_parser/yaml_parser_logging_test"
+
+class YamlParserInvalidStringTest < Test::Unit::TestCase
+  include YamlParserLoggingTest
+
+  def setup
+    @expected = [
+      { :level => "error", :message => "YAML executor definition is incorrectly formed" }
+    ]
+    @yaml_string = "
+      invalid yaml string
+    "
+  end
+end
---
+++ test/yaml_parser/yaml_parser_invalid_yaml_test.rb
@@ -0,0 +1,14 @@
+require "yaml_parser/yaml_parser_logging_test"
+
+class YamlParserInvalidYamlTest < Test::Unit::TestCase
+  include YamlParserLoggingTest
+
+  def setup
+    @expected = [
+      { :level => "error", :message => "YAML executor definition is incorrectly formed" }
+    ]
+    @yaml_string = "
+      invalid_yaml: true
+    "
+  end
+end
