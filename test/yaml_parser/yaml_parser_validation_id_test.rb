40b013d9dc0863342b138b76d9b1e171b2f78db4
Phil Ostler <philostler@gmail.com>
2011-02-07 02:06:01 +0000

    Initial commit.

---
+++ test/yaml_parser/yaml_parser_validation_id_test.rb
@@ -0,0 +1,23 @@
+require "yaml_parser/yaml_parser_logging_test"
+
+class YamlParserValidationIdTest < Test::Unit::TestCase
+  include YamlParserLoggingTest
+
+  def setup
+    @expected = [
+      { :level => "warn", :message => "YAML executor definition does not have an id. Skipping" },
+      { :level => "warn", :message => "YAML executor definition does not have an id. Skipping" },
+      { :level => "warn", :message => "YAML executor definition id must be of type String. Skipping" },
+      { :level => "warn", :message => "YAML executor definition id must be of type String. Skipping" },
+      { :level => "warn", :message => "YAML executor definition for id \"duplicate\" has already been defined. Duplicates not allowed. Skipping" }
+    ]
+    @yaml_string = "
+      - no_id: no_id
+      - id:
+      - id: true
+      - id: 0
+      - id: duplicate
+      - id: duplicate
+"
+  end
+end
