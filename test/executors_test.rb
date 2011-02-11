09edea0ecca8e289190d1b8b91423ab7b1173b07
Phil Ostler <philostler@gmail.com>
2011-02-11 19:42:06 +0000

    Added test requires.

--- test/executors_test.rb
+++ test/executors_test.rb
@@ -1,3 +1,17 @@
-require "test_helper"
+require "executors/factory_test"
+require "executors/services_test"
 
-require "factory_test"
+require "executors/configuration/yaml/configurator_test"
+
+require "executors/configuration/yaml/executor/empty_test"
+require "executors/configuration/yaml/executor/invalid_string_test"
+require "executors/configuration/yaml/executor/invalid_yaml_test"
+require "executors/configuration/yaml/executor/validation_id_test"
+require "executors/configuration/yaml/executor/validation_size_test"
+require "executors/configuration/yaml/executor/validation_type_test"
+
+require "executors/configuration/yaml/task/validation_class_test"
+require "executors/configuration/yaml/task/validation_delay_test"
+require "executors/configuration/yaml/task/validation_initial_test"
+require "executors/configuration/yaml/task/validation_required_elements_test"
+require "executors/configuration/yaml/task/validation_units_test"
