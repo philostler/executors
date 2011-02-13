ea81c56b79af3d29fd0dd9d400ddff710d6a63c3
Phil Ostler <philostler@gmail.com>
2011-02-13 16:35:22 +0000

    Initial commit.

---
+++ test/executors/configuration/yaml/executor/validation_id_test.rb
@@ -0,0 +1,25 @@
+require "executors/configuration/yaml/configurator_logging_test"
+
+class ValidationIdTest < Test::Unit::TestCase
+  include ConfiguratorLoggingTest
+
+  def setup
+    @expected = [
+      { :level => "warn", :message => "Loading YAML executor definition. \"id\" is missing. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition. \"id\" is missing. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition. \"id\" must be a string. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition. \"id\" must be a string. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition. \"id\" of \"duplicate\" has already been defined. Duplicates not allowed. Skipping" }
+    ]
+    @yaml_string = "
+      - no_id: no_id
+      - id:
+      - id: true
+      - id: 0
+      - id: duplicate
+        type: single
+      - id: duplicate
+        type: single
+"
+  end
+end
