9e54af881e865ee3eff4b701b2cd4c886e104b03
Phil Ostler <philostler@gmail.com>
2011-02-14 00:19:52 +0000

    Initial commit.

---
+++ test/executors/configuration/yaml/executor/validation_size_test.rb
@@ -0,0 +1,36 @@
+require "executors/configuration/yaml/configurator_logging_test"
+
+class ValidationSizeTest < Test::Unit::TestCase
+  include ConfiguratorLoggingTest
+
+  def setup
+    @expected = [
+      { :level => "warn", :message => "Loading YAML executor definition \"identifier\". \"size\" is missing. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition \"identifier\". \"size\" is missing. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition \"identifier\". \"size\" must be a number. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition \"identifier\". \"size\" must be larger than 0. Skipping" },
+      { :level => "warn", :message => "Loading YAML executor definition \"identifier\". \"size\" must be a number. Skipping" },
+      { :level => "info", :message => "Loading YAML executor definition \"identifier\". \"size\" is not required. Ignored" }
+    ]
+    @yaml_string = "
+      - id: identifier
+        type: fixed
+        no_size: no_size
+      - id: identifier
+        type: fixed
+        size:
+      - id: identifier
+        type: fixed
+        size: true
+      - id: identifier
+        type: fixed
+        size: 0
+      - id: identifier
+        type: fixed
+        size: size
+      - id: identifier
+        type: single
+        size: 1
+"
+  end
+end
