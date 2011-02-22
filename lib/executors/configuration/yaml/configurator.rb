3617f550b25ae5365bb4c3a66e20bbb442eb5245
Phil Ostler <philostler@gmail.com>
2011-02-22 00:55:48 +0000

    Changed to use Kwalify.

--- lib/executors/configuration/yaml/configurator.rb
+++ lib/executors/configuration/yaml/configurator.rb
@@ -1,12 +1,16 @@
-require "executors/configuration/yaml/executor"
-require "yaml"
+require "rubygems"
+require "kwalify"
+require "executors/configuration/yaml/validator"
 
 # YAML configurator.
 module Executors
   module Configuration
     module Yaml
       module Configurator
-        include Executor
+        ID_KEY = "id"
+        TYPE_KEY = "type"
+        SIZE_KEY = "size"
+        SIZE_REQUIRING_TYPES = [ "fixed", "scheduled" ]
 
         # Loads a YAML document from the specified location and attempts instantiation from the contained configuration definition.
         #
@@ -14,8 +18,7 @@ module Executors
         #
         # <code>Executors::Services.load_yaml_file Rails.root.join("config", "executors.yml")</code>
         def load_yaml_file file
-          yaml = YAML.load_file file
-          load_yaml yaml
+          load_yaml "parse_file", file
         end
 
         # Loads a YAML from the specified <code>String</code> and attempts instantiation from the contained configuration definition.
@@ -24,24 +27,51 @@ module Executors
         #
         # <code>Executors::Services.load_yaml_string YAML.load_file(Rails.root.join("config", "executors.yml"))</code>
         def load_yaml_string string
-          yaml = YAML.load string
-          load_yaml yaml
+          load_yaml "parse_stream", string
         end
 
         private
-        def load_yaml yaml
-          unless yaml
-            logger.error { "Loading YAML document. Document is empty. Aborting" } unless logger.nil?
-            return nil
+        def load_yaml method, value
+          parser = create_parser
+          document = parser.send(method, value)
+
+          unless parser_errors? parser
+            if document
+              document.each do |e|
+                create_executor e[ID_KEY], e[TYPE_KEY], e[SIZE_KEY]
+              end
+            else
+              logger.error { "Loading YAML document. Document is empty. Aborting" } unless logger.nil?
+            end
           end
-          unless yaml.is_a? Array
-            logger.error { "Loading YAML document. Document is incorrectly formed. Aborting" } unless logger.nil?
-            return nil
+        end
+        def create_parser
+          validator = Validator.new
+          parser = Kwalify::Yaml::Parser.new validator
+
+          return parser
+        end
+        def parser_errors? parser
+          parser_errors = parser.errors
+          errors = (parser_errors and not parser_errors.empty?)
+
+          if errors
+            parser_errors.each do |e|
+              depth = e.path.split("/").size
+
+              logger.error { "Loading YAML document. Error encountered on line " + e.linenum.to_s + ". " + e.message + " Aborting" } unless logger.nil?
+            end
           end
 
-          yaml.each do |y|
-            load_executor_yaml y
+          return errors
+        end
+        def create_executor id, type, size
+          unless not get(id)
+            logger.error { "Loading YAML executor definition. \"id\" of \"" + id + "\" has already been defined. Duplicates not allowed. Skipping" } unless logger.nil?; next
           end
+
+          executor = Executors::Factory.create_executor type, size
+          add id, executor
         end
       end
     end
