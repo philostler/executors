123b6adead9dee14b91dd7d535d3b6d369b04c9f
Phil Ostler <philostler@gmail.com>
2011-02-23 12:51:04 +0000

    Removed unneeded test and rename.

--- test/executors/configurators/yaml/executor/id_duplicate_test.rb
+++ test/executors/configurators/yaml/executor/id_duplicate_test.rb
@@ -1,15 +1,13 @@
 require "executors/configurators/configurator_logging_test"
 
-class IdTest < Test::Unit::TestCase
+class IdDuplicateTest < Test::Unit::TestCase
   include ConfiguratorLoggingTest
 
   def setup
     @method = "load_yaml_file"
     @files = [ File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + ".yml",
-               File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + "_duplicate.yml",
-               File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + "_duplicate_2.yml"]
+               File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + "_2.yml" ]
     @logs = [
-      { :level => "error", :message => "Loading YAML document. Error encountered on line 3. 'duplicate': is already used at '/0/id'. Aborting" },
       { :level => "error", :message => "Loading YAML executor definition. \"id\" of \"duplicate\" has already been defined. Duplicates not allowed. Skipping" }
     ]
   end
