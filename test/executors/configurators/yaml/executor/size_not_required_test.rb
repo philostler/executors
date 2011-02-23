7a4f4a7f4ee2bfe717500e513b876af474fa834c
Phil Ostler <philostler@gmail.com>
2011-02-23 12:54:09 +0000

    Removed unneeded test and rename.

--- test/executors/configurators/yaml/executor/size_not_required_test.rb
+++ test/executors/configurators/yaml/executor/size_not_required_test.rb
@@ -7,7 +7,6 @@ class SizeTest < Test::Unit::TestCase
     @method = "load_yaml_file"
     @files = [ File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + ".yml" ]
     @logs = [
-      { :level => "error", :message => "Loading YAML document. Error encountered on line 3. '0': too small (< min 1). Aborting" },
       { :level => "warn", :message => "Loading YAML document. Error encountered on line 4. \"size\" is not required. Aborting" }
     ]
   end
