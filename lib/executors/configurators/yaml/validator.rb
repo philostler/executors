59d8049875466ed1a259f818b002587da87e85d3
Phil Ostler <philostler@gmail.com>
2011-02-24 00:48:30 +0000

    Extended validations.

--- lib/executors/configurators/yaml/validator.rb
+++ lib/executors/configurators/yaml/validator.rb
@@ -5,9 +5,16 @@ module Executors
   module Configurators
     module Yaml
       class Validator < Kwalify::Validator
-        EXECUTOR_RULE_NAME = "executor"
+        EXECUTOR_TYPE_RULE = "type"
+        EXECUTOR_SIZE_RULE = "size"
+        TASK_CLASS_RULE = "class"
+        TASK_SCHEDULE_RULE = "schedule"
+        TASK_START_RULE = "start"
+        TASK_EVERY_RULE = "every"
 
         @@schema = Kwalify::Yaml.load_file(File.join(File.dirname(__FILE__), "schema.yml"))
+
+        attr_reader :type
 
         def initialize
           super(@@schema)
@@ -15,11 +22,29 @@ module Executors
 
         def validate_hook value, rule, path, errors
           case rule.name
-            when EXECUTOR_RULE_NAME
-              unless Configurator::SIZE_REQUIRING_TYPES.include? value[Configurator::TYPE_KEY].downcase
-                if value[Configurator::SIZE_KEY]
-                  errors << ValidationWarn.new("\"size\" is not required.", path, rule, value)
-                end
+            when EXECUTOR_TYPE_RULE
+              type = value.downcase
+            when EXECUTOR_SIZE_RULE
+              unless Configurator::SIZE_REQUIRING_TYPES.include? type
+                errors << ValidationWarn.new("\"size\" is not required.", path, rule, value)
+              end
+            when TASK_CLASS_RULE
+              begin
+                Object.const_get(value).new
+              rescue NameError
+                errors << Kwalify::ValidationError.new("\"class\" must be a reference to an existing class", path, rule, value)
+              end
+            when TASK_SCHEDULE_RULE
+              unless Configurator::TYPE_SCHEDULED.include? type
+                errors << ValidationWarn.new("\"schedule\" is not required", path, rule, value)
+              end
+            when TASK_START_RULE
+              unless Configurator::TYPE_SCHEDULED.include? type
+                errors << ValidationWarn.new("\"start\" is not required", path, rule, value)
+              end
+            when TASK_EVERY_RULE
+              unless Configurator::TYPE_SCHEDULED.include? type
+                errors << ValidationWarn.new("\"every\" is not required", path, rule, value)
               end
           end
         end
