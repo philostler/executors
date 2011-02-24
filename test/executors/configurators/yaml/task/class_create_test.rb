5d8baef8d95611bfb02e2018fa47b13bf3682a4b
Phil Ostler <philostler@gmail.com>
2011-02-24 00:52:05 +0000

    Change of expected log messages.

--- test/executors/configurators/yaml/task/class_create_test.rb
+++ test/executors/configurators/yaml/task/class_create_test.rb
@@ -7,7 +7,8 @@ class ClassCreateTest < Test::Unit::TestCase
     @method = "load_yaml_file"
     @files = [ File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + ".yml" ]
     @logs = [
-      { :level => "error", :message => "Implementing YAML document. \"class\" must be a reference to an existing class. Skipping" }
+      { :level => "error", :message => "Validating YAML document. The following validation error occurred on line 4 => \"class\" must be a reference to an existing class" },
+      { :level => "error", :message => "Validating YAML document. Validation error(s) occurred while loading document. Aborting" }
     ]
   end
 end
