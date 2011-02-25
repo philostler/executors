1ccf8e66b21d9946c65b08441bf92d8cbe424c4a
Phil Ostler <philostler@gmail.com>
2011-02-25 01:27:15 +0000

    Added expected log messages.

--- test/executors/configurators/yaml/executor/id_duplicate_test.rb
+++ test/executors/configurators/yaml/executor/id_duplicate_test.rb
@@ -8,7 +8,9 @@ class IdDuplicateTest < Test::Unit::TestCase
     @files = [ File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + ".yml",
                File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + "_2.yml" ]
     @logs = [
-      { :level => "error", :message => "Implementing YAML document. \"id\" of \"duplicate\" has already been defined. Duplicates not allowed. Skipping" }
+      { :level => "info", :message => "Completed YAML document. Loaded 1 executor(s) and 0 task(s)" },
+      { :level => "error", :message => "Validating YAML document. The following validation error occurred on line 1 => \"duplicate\" has already been defined. Duplicates not allowed" },
+      { :level => "error", :message => "Validating YAML document. 1 validation error(s) occurred while loading document. Aborting" }
     ]
   end
 end
