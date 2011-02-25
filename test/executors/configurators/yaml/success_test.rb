6dde3e56f25f615ea8b8fcb4b402e4e8265bbc33
Phil Ostler <philostler@gmail.com>
2011-02-25 01:27:51 +0000

    Initial commit.

---
+++ test/executors/configurators/yaml/success_test.rb
@@ -0,0 +1,13 @@
+require "executors/configurators/configurator_logging_test"
+
+class SuccessTest < Test::Unit::TestCase
+  include ConfiguratorLoggingTest
+
+  def setup
+    @method = "load_yaml_file"
+    @files = [ File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + ".yml" ]
+    @logs = [
+      { :level => "info", :message => "Completed YAML document. Loaded 2 executor(s) and 2 task(s)" }
+    ]
+  end
+end
