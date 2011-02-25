190dfa6535ddeaa823c3524f098c0685d4f6822b
Phil Ostler <philostler@gmail.com>
2011-02-25 01:28:19 +0000

    Changed and added expected log messages.

--- test/executors/configurators/yaml/empty_test.rb
+++ test/executors/configurators/yaml/empty_test.rb
@@ -7,7 +7,8 @@ class EmptyTest < Test::Unit::TestCase
     @method = "load_yaml_file"
     @files = [ File.join(File.dirname(__FILE__), self.class.name.split(/(?=[A-Z])/).join("_").downcase) + ".yml" ]
     @logs = [
-      { :level => "error", :message => "Validating YAML document. Document is empty. Aborting" }
+      { :level => "error", :message => "Validating YAML document. The following validation error occurred on line 1 => Empty document" },
+      { :level => "error", :message => "Validating YAML document. 1 validation error(s) occurred while loading document. Aborting" }
     ]
   end
 end
