d3471825da192f9d0e136f82005650f3df150756
Phil Ostler <philostler@gmail.com>
2011-02-25 01:32:22 +0000

    All validations now refactored into this class.

--- lib/executors/configurators/yaml/validator.rb
+++ lib/executors/configurators/yaml/validator.rb
@@ -5,6 +5,8 @@ module Executors
   module Configurators
     module Yaml
       class Validator < Kwalify::Validator
+        DOCUMENT_RULE = "document"
+        EXECUTOR_ID_RULE = "id"
         EXECUTOR_TYPE_RULE = "type"
         EXECUTOR_SIZE_RULE = "size"
         TASK_CLASS_RULE = "class"
@@ -12,9 +14,12 @@ module Executors
         TASK_START_RULE = "start"
         TASK_EVERY_RULE = "every"
 
+        SCHEDULABLE_EXECUTOR_TYPES = [ "scheduled", "single_scheduled" ]
+        EXECUTOR_TYPES_REQUIRING_SIZE = [ "fixed", "scheduled" ]
+
         @@schema = Kwalify::Yaml.load_file(File.join(File.dirname(__FILE__), "schema.yml"))
 
-        attr_reader :type
+        attr_reader :type, :start
 
         def initialize
           super(@@schema)
@@ -22,10 +27,18 @@ module Executors
 
         def validate_hook value, rule, path, errors
           case rule.name
+            when DOCUMENT_RULE
+              unless value
+                errors << Kwalify::ValidationError.new("Empty document", path, rule, value)
+              end
+            when EXECUTOR_ID_RULE
+              unless not Executors::Services.get value
+                errors << Kwalify::ValidationError.new("\"" + value + "\" has already been defined. Duplicates not allowed", path, rule, value)
+              end
             when EXECUTOR_TYPE_RULE
-              type = value.downcase
+              @type = value.downcase
             when EXECUTOR_SIZE_RULE
-              unless Configurator::SIZE_REQUIRING_TYPES.include? type
+              unless EXECUTOR_TYPES_REQUIRING_SIZE.include? @type
                 errors << ValidationWarn.new("\"size\" is not required.", path, rule, value)
               end
             when TASK_CLASS_RULE
@@ -35,16 +48,22 @@ module Executors
                 errors << Kwalify::ValidationError.new("\"class\" must be a reference to an existing class", path, rule, value)
               end
             when TASK_SCHEDULE_RULE
-              unless Configurator::TYPE_SCHEDULED.include? type
+              unless SCHEDULABLE_EXECUTOR_TYPES.include? @type
                 errors << ValidationWarn.new("\"schedule\" is not required", path, rule, value)
               end
             when TASK_START_RULE
-              unless Configurator::TYPE_SCHEDULED.include? type
+              @start = value
+              unless SCHEDULABLE_EXECUTOR_TYPES.include? @type
                 errors << ValidationWarn.new("\"start\" is not required", path, rule, value)
               end
             when TASK_EVERY_RULE
-              unless Configurator::TYPE_SCHEDULED.include? type
+              unless SCHEDULABLE_EXECUTOR_TYPES.include? @type
                 errors << ValidationWarn.new("\"every\" is not required", path, rule, value)
+              end
+              if start
+                unless start.split(".")[1] == value.split(".")[1]
+                  errors << Kwalify::ValidationError.new("\"start\" and \"every\" must have matching units", path, rule, value)
+                end
               end
           end
         end
